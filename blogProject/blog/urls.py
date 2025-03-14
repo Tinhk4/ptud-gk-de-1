053d52fa7a58499e6dcad07455bb2317624c6b08
Tinhk4 <thantrongtinh12a1@gmail.com>
2025-03-14 20:03:09 +0700

    Update: comment

--- blogProject/blog/urls.py
+++ blogProject/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('post/new/', views.create_post, name='create_post'),
     path('post/edit/<int:pk>/', views.edit_post, name='edit_post'),
     path('post/delete/<int:pk>/', views.delete_post, name='delete_post'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), 
 
     path('register/', views.register, name='register'),
 ]
--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import permission_required, login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 from .forms import RegisterForm
 from django.contrib.auth import login
@@ -62,4 +62,19 @@ def register(request):
 
     return render(request, 'blog/register.html', {'form': form})
 
+@login_required
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
 
